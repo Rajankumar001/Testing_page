2641e3769b5281531ff921f243ac0bc3962781af
PrabhavNasa <142142259+PrabhavNasa@users.noreply.github.com>
2025-01-17 14:17:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const button = document.querySelector('.handle_event');
-    button.addEventListener('click', function() {
-        window.location.href = 'https://learn.angelsonearthhub.com/web/apps/e2445eeb-3c9f-49a7-8c1a-b3879d8149c2';
+    button.addEventListener('click', async function () {
+        try {
+            const response = await fetch('https://learn.angelsonearthhub.com/web/apps/e2445eeb-3c9f-49a7-8c1a-b3879d8149c2');
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            const data = await response.json(); // Parse JSON data if the response is JSON
+            console.log('Fetched Data:', data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
     });
 });
