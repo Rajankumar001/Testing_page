d24aa8d8f3b93c36d2810a785c0323bbc0fc3d83
Rajankumar001 <chaudharyrajan387@gmail.com>
2025-01-17 13:51:13 +0530

    initial testing

---
+++ index.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.querySelector('.handle_event');
+    button.addEventListener('click', function() {
+        window.location.href = 'https://learn.angelsonearthhub.com/web/apps/e2445eeb-3c9f-49a7-8c1a-b3879d8149c2';
+    });
+});
